b0d4aa4140006c4a2ea128c474f6cc7571d567aa
Gaurav Ghimire <exsys@nixify.com>
2015-01-30 23:17:36 +0545

    Bugfixes on user signup  - Created new separate form for user signup  - Updated tests  - Added a manage link on superuser home portal  - Moved from http -> https for remote css/js (Browser compatible for SSL)  - Moved couple of API features to local dev only

--- api/urls.py
+++ api/urls.py
@@ -8,18 +8,18 @@ from rest_framework.urlpatterns import format_suffix_patterns
 # from rest_framework.authtoken import views
 urlpatterns = patterns('',
     # Examples:
-    url(r'^api-auth/$', apiview.obtain_auth_token),
-    url(r'^users/(?P<pk>\w+)/$', apiview.UsersDetail.as_view()),
-    url(r'^docs/', include('rest_framework_swagger.urls')),
     url(r'^jobs/$', apiview.JobsDetail.as_view()),
     url(r'^jobs/(?P<pk>\w+)/$', apiview.JobDetail.as_view()),
-    url(r'^verify/$', apiview.VerifyPhone.as_view(), name='verifyPhone'),
 )
 
 if settings.DEBUG:
     urlpatterns += patterns('',
+        url(r'^api-auth/$', apiview.obtain_auth_token),
+        url(r'^docs/', include('rest_framework_swagger.urls')),
+        url(r'^users/(?P<pk>\w+)/$', apiview.UsersDetail.as_view()),
 		url(r'^signup/$', apiview.UserSignup.as_view()),
 		url(r'^users/$', apiview.UsersList.as_view()),
+        url(r'^verify/$', apiview.VerifyPhone.as_view(), name='verifyPhone'),
         ) 
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- thm/urls.py
+++ thm/urls.py
@@ -6,13 +6,9 @@ from django.conf.urls import patterns, include, url
 from users import views as userviews
 from jobs import views as jobviews
 from .views import index, manage
+import logging
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'thm.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    # url(r'^admin/', include(admin.site.urls)),
     url(r'^signin/$', userviews.signin, name='signin'),
     url(r'^logout/$', userviews.logout, name='logout'),
     url(r'^home/$', userviews.home, name='home'),
@@ -24,7 +20,6 @@ urlpatterns = patterns('',
     url(r'^4DEa6cvdaP0/$', userviews.gaTracker, name='gaTracker'),
     url(r'^verify/$', userviews.verifyPhone, name='verifyPhone'),
     url(r'^sendvrfcode/$', userviews.sendVrfCode, name='sendVrfCode'),
-    url(r'^myprofile/$', userviews.myProfile, name='myProfile'),
     url(r'^faq/', include('faq.urls')),
     url(r'^jobs/', include('jobs.urls')),
     url(r'^manage/$', manage, name='manage'),
@@ -35,6 +30,7 @@ if settings.DEBUG:
     urlpatterns += patterns('',
     url(r'^api/v1/', include('api.urls') ),
     url(r'^signup/$', userviews.signup, name='signup'),
+    # url(r'^profile/$', userviews.myProfile, name='myProfile'),
     ) 
 
 urlpatterns += patterns('',
