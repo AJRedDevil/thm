cf5591b7c4ea7098c6ad43121b8f49e71b617165
Gaurav Ghimire <exsys@nixify.com>
2015-04-13 17:23:35 +0545

    Quickfix jobtype length

--- jobs/models.py
+++ jobs/models.py
@@ -71,7 +71,7 @@ class Jobs(models.Model):
         default=timezone.now)
     jobtype = models.CharField(
         _('jobtype'),
-        max_length=250,
+        max_length=1,
         choices=JOBS_SELECTION,
         default='0',)
     handyman = models.ManyToManyField(UserProfile, related_name='orders')
