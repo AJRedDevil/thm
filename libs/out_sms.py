410ef2f3389f08aa8f87c122127d58460dd03e9f
Gaurav Ghimire <exsys@nixify.com>
2015-08-19 10:05:30 +0545

    Hotfix : corrected phone number

--- libs/out_sms.py
+++ libs/out_sms.py
@@ -7,6 +7,6 @@ JOB_REQ_MSG='We have received your repair request and would get back to you soon
 DUP_USER_REG_MSG='Your account request is being processed. We would notify you once your account is active!'
 PHONE_VERF_MSG='Your phone verification code is {0}. Thank you, Homerepair.'
 SEND_PASSWD_MSG='Your password reset code is : {0} . If you did not request for one, please ignore. Thank you, Homerepair.'
-USER_WELCOME_MSG='For any repair requests, please SMS REPAIR to 2200 OR call us at 9801195400 / 01553225. Thank you, Homerepair.'
+USER_WELCOME_MSG='For any repair requests, please SMS REPAIR to 2200 OR call us at 9801195400 / 015532225. Thank you, Homerepair.'
 JOB_ACCEPTED_MSG='Our repair expert {0} ({1}) has been assigned for your request! Thank you, Homerepair.'
 JOB_COMPLETE_MSG='Your request ID:{0} has been completed! Thank you, Homerepair.'
